f2ff31717e59c4d4d6227cdc11d3853dee76c219
Vojtěch Sázel <vojtech@sazel.cz>
2016-01-08 14:25:38 +0100

    changes for kotlin 1.0 beta4

--- hello_android/src/main/kotlin/cz/sazel/hellokotlin/MainActivity.kt
+++ hello_android/src/main/kotlin/cz/sazel/hellokotlin/MainActivity.kt
@@ -1,12 +1,10 @@
 package cz.sazel.hellokotlin
 
 
-import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.TextView
-import android.widget.VideoView
 import butterknife.Bind
 import butterknife.ButterKnife
 import butterknife.OnClick
@@ -40,7 +38,7 @@ class MainActivity : AppCompatActivity() {
     protected fun startClick(v: View) {
         sharedClass.printMe()
         //we can't have direct sharedClass.printPrimes(1000) there, we need to do it asynchronously to not block the UI
-        async {
+        async() {
             val primes=sharedClass.calcPrimes(1000);
             val output=primes.map { prime -> prime.toString() }.reduce { s1, s2 -> s1+"\n"+s2 }
             uiThread {
