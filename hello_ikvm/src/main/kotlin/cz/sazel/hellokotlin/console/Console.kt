4be5c09e98fbfc985ca2f41e4d934f87f3207abc
Vojtěch Sázel <vojtech@sazel.cz>
2016-03-16 14:45:13 +0100

    some fixes

--- hello_ikvm/src/main/kotlin/cz/sazel/hellokotlin/console/Console.kt
+++ hello_ikvm/src/main/kotlin/cz/sazel/hellokotlin/console/Console.kt
@@ -1,5 +1,7 @@
 package cz.sazel.hellokotlin.console
 
+import cli.System.Console
+
 /**
  * Console for Java console version.
  */
@@ -7,7 +9,8 @@ class Console:IConsole {
 
     override fun println(s: String) {
         //just print text to output
-        cli.System.Console.WriteLine(s);
+
+        Console.WriteLine(s);
 
     }
 }
