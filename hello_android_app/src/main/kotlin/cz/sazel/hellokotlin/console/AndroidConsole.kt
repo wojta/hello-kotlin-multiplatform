245a3449dbc29ed420800e51645e2632422d4fda
Vojtěch Sázel <vojtech@sazel.cz>
2018-01-25 23:39:22 +0100

    fixed note on AndroidConsole

--- hello_android_app/src/main/kotlin/cz/sazel/hellokotlin/console/AndroidConsole.kt
+++ hello_android_app/src/main/kotlin/cz/sazel/hellokotlin/console/AndroidConsole.kt
@@ -3,7 +3,7 @@ package cz.sazel.hellokotlin.console
 import android.widget.TextView
 
 /**
- * Created by wojta on 22.12.15.
+ * Android specific console implementation.
  */
 class AndroidConsole(val textView: TextView) : IConsole {
     override fun println(s: String) {
