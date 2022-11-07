d5a88f088b183d5c995068ab00925e0f4438eb72
Vojtěch Sázel <wojta@users.noreply.github.com>
2022-11-08 00:24:37 +0100

    platforms added

--- hello_console_app/build.gradle.kts
+++ hello_console_app/build.gradle.kts
@@ -1,16 +1,27 @@
+import org.jetbrains.kotlin.com.intellij.openapi.util.SystemInfo.isLinux
+import org.jetbrains.kotlin.com.intellij.openapi.util.SystemInfo.isWindows
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
 plugins {
     kotlin("multiplatform")
 }
 
+
 kotlin {
 
-    listOf(
-        linuxX64(),
-        linuxArm64(),
-//        mingwX86(),
-//        mingwX64()
-    ).forEach {
-        it.binaries  {
+    val platforms = mutableListOf<KotlinNativeTarget>()
+    if (isLinux) {
+        platforms += linuxArm64()
+        platforms += linuxX64()
+    }
+
+    if (isWindows) {
+        platforms += mingwX86()
+        platforms += mingwX64()
+    }
+
+    platforms.forEach {
+        it.binaries {
             executable("hello_console_app") {
                 entryPoint = "cz.sazel.hellokotlin.main"
             }
@@ -30,21 +41,23 @@ kotlin {
             }
         }
 
-
-        val linuxArm64Main by getting
-        val linuxX64Main by getting
-//        val mingwX86Main by getting
-//        val mingwX64Main by getting
-
         val consoleMain by creating {
             dependsOn(commonMain)
             dependencies {
                 implementation(project(":hello_shared"))
             }
-            linuxX64Main.dependsOn(this)
-            linuxArm64Main.dependsOn(this)
-//            mingwX86Main.dependsOn(this)
-//            mingwX64Main.dependsOn(this)
+            if (isLinux) {
+                val linuxArm64Main by getting
+                val linuxX64Main by getting
+                linuxX64Main.dependsOn(this)
+                linuxArm64Main.dependsOn(this)
+            }
+            if (isWindows) {
+                val mingwX86Main by getting
+                val mingwX64Main by getting
+                mingwX86Main.dependsOn(this)
+                mingwX64Main.dependsOn(this)
+            }
         }
     }
 }
